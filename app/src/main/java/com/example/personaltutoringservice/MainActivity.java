1351ef342e4b69877fe4f04c6364365176c14b83
AaronnRoycee <aaron.pantoja@mavs.uta.edu>
2026-04-01 21:03:40 -0500

    Attempting to create the registration and login buttons, right now i just have it do a pop up when you click the buttons saying "button clicked".

--- app/src/main/java/com/example/personaltutoringservice/MainActivity.java
+++ app/src/main/java/com/example/personaltutoringservice/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.personaltutoringservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //register button - testing screen setup and a return when you click on button
+        Button registerButton = findViewById(R.id.register);
+
+        registerButton.setOnClickListener(v -> {
+                    Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+        });
+
+        //login button - testing screen setup and a return when you click on button
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+        });
+
+    };
+};
