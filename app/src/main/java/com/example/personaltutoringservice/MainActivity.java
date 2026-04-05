7d9a793e6cb804a092cead07c318076a2adc7dfe
CareVR2022 <112016763+CareVR2022@users.noreply.github.com>
2026-04-05 15:34:07 -0500

    Registration/login updates

--- app/src/main/java/com/example/personaltutoringservice/MainActivity.java
+++ app/src/main/java/com/example/personaltutoringservice/MainActivity.java
@@ -5,12 +5,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Intent;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etUsername, etPassword; //user input for these text fields
+    EditText etUsername, etPassword, etEmail; //user input for these text fields
     Button btnLogin, btnRegister, btnForgot; //current buttons on homepage
 
     @Override
@@ -18,20 +19,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUsername = findViewById(R.id.etUsername);
+        //etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
 
         btnLogin = findViewById(R.id.btnLogin); //login button
         btnRegister = findViewById(R.id.btnRegister); //register button
         btnForgot = findViewById(R.id.btnForgot); //forgot username or password button
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
         //LOGIN FUNCTIONS
         btnLogin.setOnClickListener(v -> {
-            String username = etUsername.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
             String password = etPassword.getText().toString().trim();
                 //when button is clicked, get what the user type for username and password and returns to a string (trim removes extra spaces at beginning/end)
 
-            if (username.isEmpty() || password.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+
+                        if(task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            if(user.isEmailVerified()) {
+
+                                Toast.makeText(this,"Login Successful",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(this, MainActivity.class));
+
+                            } else {
+                                Toast.makeText(this,"Please verify your email",Toast.LENGTH_LONG).show();
+                            }
+
+                        } else {
+                            Toast.makeText(this,"Login Failed",Toast.LENGTH_LONG).show();
+                        }
+
+                    });
+
+            if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
             } else {
                 Intent loginIntent = new Intent(MainActivity.this, HomePageActivity.class);
@@ -44,8 +70,27 @@ public class MainActivity extends AppCompatActivity {
 
         //REGISTER FUNCTIONS
         btnRegister.setOnClickListener(v -> {
-            Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(registerIntent);
+
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+
+                        if(task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            user.sendEmailVerification();
+
+                            Toast.makeText(this,"Verification Email Sent",Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(this,"Registration Failed",Toast.LENGTH_LONG).show();
+                        }
+
+                    });
+//            Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+//            startActivity(registerIntent);
         });
                 //send user to register page
 
