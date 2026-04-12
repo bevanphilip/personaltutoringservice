5062108ff1ed11d6757ff25a8b392cf3d58c0e34
AaronnRoycee <aaron.pantoja@mavs.uta.edu>
2026-04-12 14:28:57 -0500

    committing to my branch: working on the main home page GUI and going to pull in registration and login from other branches

--- app/src/main/java/com/example/personaltutoringservice/HomePageActivity.java
+++ app/src/main/java/com/example/personaltutoringservice/HomePageActivity.java
@@ -1,14 +1,61 @@
 package com.example.personaltutoringservice;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // update the username
+        TextView welcomeText = findViewById(R.id.textWelcome);
+
+        String username = "User"; // temporary (later from login)
+        welcomeText.setText("Welcome, " + username);
+
+        TextView profile = findViewById(R.id.linkMyProfile);
+        TextView featured = findViewById(R.id.linkFeaturedTutors);
+        TextView myTutors = findViewById(R.id.linkMyTutors);
+
+        Button findTutor = findViewById(R.id.buttonFindTutor);
+        Button becomeTutor = findViewById(R.id.buttonBecomeTutor);
+
+// Profile page
+        profile.setOnClickListener(v -> {
+            startActivity(new Intent(this, ProfileActivity.class));
+        });
+
+// Search tutors page
+        featured.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchTutorsActivity.class));
+        });
+
+        findTutor.setOnClickListener(v -> {
+            startActivity(new Intent(this, SearchTutorsActivity.class));
+        });
+
+// My Tutors page
+        myTutors.setOnClickListener(v -> {
+            startActivity(new Intent(this, MyTutorsActivity.class));
+        });
+
+// Become tutor page
+        becomeTutor.setOnClickListener(v -> {
+            startActivity(new Intent(this, BecomeTutorActivity.class));
+        });
+
+
+
     }
 }
